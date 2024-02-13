877c4dc55b7696006f42543df5a6d949b12b52c4
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-02-14 01:27:52 +0200

    calculate and display total in, out anf interest

--- script.js
+++ script.js
@@ -82,6 +82,26 @@ const calcDisplayBalance = function (movements) {
 };
 calcDisplayBalance(account1.movements);
 
+const calcDisplaySummary = (movements) => {
+  const incomes = movements
+    .filter((mov) => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}$`;
+
+  const out = movements
+    .filter((mov) => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}$`;
+
+  const interest = movements
+    .filter((mov) => mov > 0)
+    .map((deposit) => (deposit * 1.2) / 100)
+    .filter((int) => int >= 1)
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}$`;
+};
+calcDisplaySummary(account1.movements);
+
 const createUsernames = (accs) => {
   accs.forEach(function (acc) {
     acc.username = acc.owner
